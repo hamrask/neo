b568589f0a7a3594d5a0d40add8d68715fc6cf0f
hamraz <khamras@gmail.com>
2020-02-21 19:44:00 +0530

    initial commit

--- src/app/core/home/home-routing.module.ts
+++ src/app/core/home/home-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./core/home/home.module').then( x => x.HomeModule)
+    component: HomeComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class HomeRoutingModule { }
---
+++ src/app/core/home/home.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { TitlebarComponent } from 'src/app/shared/components/titlebar/titlebar.component';
+import { FooterComponent } from 'src/app/shared/components/footer/footer.component';
+import { SidebarComponent } from 'src/app/shared/components/sidebar/sidebar.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, TitlebarComponent , FooterComponent , SidebarComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
+  ]
+})
+export class HomeModule { }
